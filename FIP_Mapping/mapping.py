2955befbdaf2713e8758d33aa8a5434fc6ed244e
bernardobap21 <bernardoap16@gmail.com>
2025-04-25 18:15:36 +0200

    Evaluation test

--- FIP_Mapping/mapping.py
+++ FIP_Mapping/mapping.py
@@ -5,6 +5,6 @@ def load_mapping(json_path='fip_madmp_mapping.json'):
         return json.load(file)
 
 # Quick verification, remove/comment out after verifying initially.
-if __name__ == "__main__":
-    mapping_data = load_mapping()
-    print(json.dumps(mapping_data, indent=4))
+# if __name__ == "__main__":
+#     mapping_data = load_mapping()
+#     print(json.dumps(mapping_data, indent=4))
